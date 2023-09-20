5ae1e9a244ff5f7e8d466d28cd2016fddf7ccf94
Matt Wright <thewrightmatt@gmail.com>
2023-09-20 03:44:03 +0000

    bucket resource

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
@@ -14,6 +19,15 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
 output "random_bucket_name" {
