cea601d3b652ac0a25fc4ca1c3194e1c6cd782f7
Matt Wright <thewrightmatt@gmail.com>
2023-09-24 15:11:08 -0500

    S3 tag, validation and vars

--- main.tf
+++ main.tf
@@ -1,31 +1,4 @@
-terraform {
-  cloud {
-    organization = "tf_bootcamp_twm"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
 
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "random_string" "bucket_name" {
   lower   = true
@@ -36,8 +9,8 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
