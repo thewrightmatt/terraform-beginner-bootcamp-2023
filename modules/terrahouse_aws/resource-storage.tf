d3a0a28f8d85147620840bda143e100a8a3f428f
Matt <thewrightmatt@users.noreply.github.com>
2023-09-29 18:12:26 -0500

    #32 only updates *.html if content vers changes (#32)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -24,6 +24,11 @@ resource "aws_s3_object" "index_html" {
   source       = var.index_html_filepath
   content_type = "text/html"
   etag         = filemd5(var.index_html_filepath)
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -32,6 +37,10 @@ resource "aws_s3_object" "error_html" {
   source       = var.error_html_filepath
   content_type = "text/html"
   etag         = filemd5(var.error_html_filepath)
+
+  lifecycle {
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -54,4 +63,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
