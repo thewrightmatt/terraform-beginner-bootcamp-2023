c56f0a5f451f58ee05af3ed88e2ccc0aa76fb4f4
Matt Wright <thewrightmatt@gmail.com>
2023-10-10 17:55:17 -0500

    #41 Initial commit

---
+++ terraform-provider-terratowns/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	// "log"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: Provider,
+	})
+	// Format.PrintLine
+	// Prints to standard output
+	fmt.Println("Hello, world!")
+}
+
+func Provider() *schema.Provider {
+	var p *schema.Provider
+	p = &schema.Provider{
+		ResourcesMap:  map[string]*schema.Resource{
+
+		},
+		DataSourcesMap:  map[string]*schema.Resource{
+
+		},
+		Schema: map[string]*schema.Schema{
+			"endpoint": {
+				Type: schema.TypeString,
+				Required: true,
+				Description: "The endpoint for hte external service",
+			},
+			"token": {
+				Type: schema.TypeString,
+				Sensitive: true, // make the token as sensitive, hide in the logs
+				Required: true,
+				Description: "Bearer token for authorization",
+			},
+			"user_uuid": {
+				Type: schema.TypeString,
+				Required: true,
+				Description: "UUID for configuration",
+				//ValidateFunc: validateUUID,
+			},
+		},
+	}
+	//p.ConfigureContextFunc = providerConfigure(p)
+	return p
+}
+
+//func validateUUID(v interface{}, k string) (ws []string, errors []error) {
+//	log.Print('validateUUID:start')
+//	value := v.(string)
+//	if _,err = uuid.Parse(value); err != nil {
+//		errors = append(error, fmt.Errorf("invalid UUID format"))
+//	}
+//	log.Print('validateUUID:end')
+//}
