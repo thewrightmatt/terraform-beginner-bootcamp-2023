8632b65e3b7b231fd115c8734de19472f062ab90
Matt <thewrightmatt@users.noreply.github.com>
2023-10-10 21:24:11 -0500

    41 terratowns provider (#43)
    * #41 Initial commit
    
    * #41 wip setting up skeleton for tf provider
    
    * #47 implement tf provider block in tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "bucket_name" {
-  description = "bucket name for our static website hosting"
-  value       = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "bucket name for our static website hosting"
+#   value       = module.terrahouse_aws.bucket_name
+# }
 
-output "cloudfront_url" {
-  description = "CF distribution domain name"
-  value       = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "CF distribution domain name"
+#   value       = module.terrahouse_aws.cloudfront_url
+# }
